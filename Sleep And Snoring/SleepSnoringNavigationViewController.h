c84f8c6d5f4b801002f4c5ef35592ccf56bd08ba
Xibo Wang <paulop5288@gmail.com>
2015-06-18 22:31:14 +0100

    Add customized navigation controller

---
+++ Sleep And Snoring/SleepSnoringNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  SleepSnoringNavigationViewController.h
+//  Sleep And Snoring
+//
+//  Created by Jiao Liu on 15/6/18.
+//  Copyright (c) 2015年 Xibo Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SleepSnoringNavigationViewController : UINavigationController
+
+@end
---
+++ Sleep And Snoring/SleepSnoringNavigationViewController.m
@@ -0,0 +1,41 @@
+//
+//  SleepSnoringNavigationViewController.m
+//  Sleep And Snoring
+//
+//  Created by Jiao Liu on 15/6/18.
+//  Copyright (c) 2015年 Xibo Wang. All rights reserved.
+//
+
+#import "SleepSnoringNavigationViewController.h"
+
+@interface SleepSnoringNavigationViewController ()
+
+@end
+
+@implementation SleepSnoringNavigationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(NSUInteger)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
